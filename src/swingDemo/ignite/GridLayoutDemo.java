7f04bc61a671d5f8312aefcd3d0f455f4d02ad0d
bharat khanal <varat.khanal468@gmail.com>
2024-06-29 16:07:39 +0545

    Grid Layout Demo

--- src/swingDemo/ignite/GridLayoutDemo.java
+++ src/swingDemo/ignite/GridLayoutDemo.java
@@ -1,24 +1,33 @@
 package swingDemo.ignite;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.GraphicsConfiguration;
 import java.awt.GridLayout;
+import java.awt.Insets;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 
 public class GridLayoutDemo {
 	public GridLayoutDemo(){
-		JFrame frame = new JFrame("First Window"); //object of frame to be displayed in desktop		
+		JFrame frame = new JFrame(); //object of frame to be displayed in desktop		
+		
 		frame.setSize(450,400);
-		//frame.setTitle("First Window");
+		frame.setTitle("First Window");
 		frame.setLocationRelativeTo(null);
 		
-		GridLayout gridLayout = new GridLayout(3,2);
+		GridLayout gridLayout = new GridLayout(3,2,30,30);
 		
 		
-		frame.setLayout(null); //to be  discussed later
+		//frame.setLayout(gridLayout); //to be  discussed later
 		
 		//objects of components
 		JButton btnLogin = new JButton("Login");	
+		JButton btnRegister = new JButton("Register");
 		JLabel lblUser = new JLabel("User Name : ");
 		lblUser.setBackground(Color.BLACK);
 		JLabel lblPass = new JLabel("Password ");
@@ -27,25 +36,36 @@ public class GridLayoutDemo {
 	
 		
 		//configured the position and alignment
-		btnLogin.setBounds(130,170,160,30); //x,y,w,h
-		lblUser.setBounds(50,50,160,30); //x,y,w,h
-		txtUser.setBounds(220,50,160,30);
-		lblPass.setBounds(50,110,160,30);
-		txtPass.setBounds(220,110,160,30);
+		btnLogin.setSize(160,30); //x,y,w,h
+		lblUser.setSize(160,30); //x,y,w,h
+		txtUser.setSize(160,30);
+		lblPass.setSize(160,30);
+		txtPass.setSize(160,30);
+		btnRegister.setSize(160,30);
 		
 		//adding components to framea
-		frame.add(lblUser);
-		frame.add(btnLogin);
-		frame.add(txtUser);
-
-		frame.add(lblPass);
-
-		frame.add(txtPass);
+		
+		
+		JPanel panel = new JPanel(gridLayout);
+		EmptyBorder eb = new EmptyBorder(30,30,30,30);
+		panel.setBorder(eb);
+		
+		//adding components to frame
+		panel.add(lblUser);	
+		panel.add(txtUser);
+		panel.add(lblPass);
+		panel.add(txtPass);
+		panel.add(btnLogin);
+		panel.add(btnRegister);
+		
+		frame.add(panel); // adding intermediatory container
 		
 		//set layout and make visible
+		frame.pack();
 		frame.setVisible(true);
 	}
 	public static void main(String[] args) {
+		new GridLayoutDemo();
 		
 	}
 }

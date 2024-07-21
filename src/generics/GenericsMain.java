69a6a40372814e0e7849f486cab90fb3b5761c5f
bharat khanal <varat.khanal468@gmail.com>
2024-07-21 10:58:04 +0545

    generic class

--- src/generics/GenericsMain.java
+++ src/generics/GenericsMain.java
@@ -1,6 +1,6 @@
 package generics;
 
-
+import java.util.LinkedList;
 
 public class GenericsMain {
 	public static void main(String[] args) {
@@ -12,30 +12,28 @@ public class GenericsMain {
 		String[] strArray = {"Hello", "World"};
 		GenericMethod.printArray(strArray);
 		
-		Book[] bookList = new Book[3];
+		Book[] bookList = new Book[3];//array of book
 		
 		bookList[0]=new Book("The Great Gatsby", "F. Scott Fitzgerald",5677, "pearson");
 		bookList[1]=new Book("To Kill a Mockingbird", "Harper Lee", 1234,"lotus");
 		bookList[2]=new Book("1984", "George Orwell",950, "pearson");
 		GenericMethod.printArray(bookList);
 		
-
-		//GenericMethod.printArray(intArray); // Outputs: 1 2 3 4 5
-		//GenericMethod.printArray(strArray); // Outputs: Hello World
-		
 		// TODO Auto-generated method stub
+		
 		// Generic Class 
-//		Box<Integer> integerBox = new Box<>(); 
-//		integerBox.set(10); 
-//		System.out.println("Integer Value: " + integerBox.get());
-//		
-//		Box<Float> floatBox = new Box<>(); 
-//		floatBox.set(8.7f); 
-//		System.out.println("Integer Value: " + integerBox.get());
-//		
-//		Box<Character> characterBox = new Box<>(); 
-//		characterBox.set('t'); 
-//		System.out.println("Integer Value: " + integerBox.get());
+		
+		Box<Integer> integerBox = new Box<>(); 
+		integerBox.set(10); 
+		System.out.println("Integer Value: " + integerBox.get());
+		
+		Box<Float> floatBox = new Box<>(); 
+		floatBox.set(8.7f); 
+		System.out.println("Float Value: " + floatBox.get());
+		
+		Box<Character> characterBox = new Box<>(); 
+		characterBox.set('t'); 
+		System.out.println("Character Value: " + characterBox.get());
 		
 		
 
